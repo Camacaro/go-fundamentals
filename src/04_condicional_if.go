f413b443a04091327305494363dc00d3dd01662a
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-10-23 20:20:50 -0300

    retos condicionales

--- src/04_condicional_if.go
+++ src/04_condicional_if.go
@@ -2,6 +2,14 @@
 
 // import "fmt"
 
+// func isPar(n int) bool {
+// 	return n%2 == 0
+// }
+
+// func login(user, password string) bool {
+// 	return user == "admin" && password == "admin"
+// }
+
 // func main() {
 
 // 	fmt.Println("============== Ejecicio 1 - if ==================")
@@ -26,5 +34,22 @@
 // 		fmt.Println("Son iguales")
 // 	} else {
 // 		fmt.Println("No son iguales")
+// 	}
+
+// 	fmt.Println("============== Reto 1 - Numero Par  ==================")
+// 	numero := 10
+// 	if isPar(numero) {
+// 		fmt.Println("El numero es par")
+// 	} else {
+// 		fmt.Println("El numero es impar")
+// 	}
+
+// 	fmt.Println("============== Reto 1 - Login  ==================")
+// 	user := "admin"
+// 	password := "admin"
+// 	if login(user, password) {
+// 		fmt.Println("Login correcto")
+// 	} else {
+// 		fmt.Println("Login incorrecto")
 // 	}
 // }
