05457090ee9630c30dc141edeea433e2d5f0fc4b
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-10-23 20:17:03 -0300

    condicional if

---
+++ src/04_condicional_if.go
@@ -0,0 +1,30 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	fmt.Println("============== Ejecicio 1 - if ==================")
+// 	valor1 := 1
+// 	valor2 := 2
+
+// 	if valor1 == valor2 {
+// 		fmt.Println("Son iguales")
+// 	} else {
+// 		fmt.Println("No son iguales")
+// 	}
+
+// 	fmt.Println("============== Ejecicio 2 - Operadores logico AND  ==================")
+// 	if valor1 == 1 && valor2 == 2 {
+// 		fmt.Println("Son iguales")
+// 	} else {
+// 		fmt.Println("No son iguales")
+// 	}
+
+// 	fmt.Println("============== Ejecicio 3 - Operadores logico OR  ==================")
+// 	if valor1 == 0 || valor2 == 2 {
+// 		fmt.Println("Son iguales")
+// 	} else {
+// 		fmt.Println("No son iguales")
+// 	}
+// }
--- src/main.go
+++ src/main.go
@@ -3,37 +3,6 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("============== Ejecicio 1  ==================")
 
-	fmt.Println("============== Ejecicio 1 - For Condicional ==================")
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
-
-	fmt.Println("============== Ejecicio 2 - For While ==================")
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
-	}
-
-	fmt.Println("============== Ejecicio 3 - For forever ==================")
-	counterForever := 0
-	for {
-		fmt.Println(counterForever)
-		counterForever++
-		if counterForever == 10 {
-			break
-		}
-	}
-
-	fmt.Println("============== Ejecicio 4 - For Range  ==================")
-	listaNumerosPares := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
-	for i, par := range listaNumerosPares {
-		fmt.Printf("posicion %d número par: %d \n", i, par)
-	}
-
-	fmt.Println("============== Reto  ==================")
-	for i := 10; i > 0; i-- {
-		fmt.Println(i)
-	}
 }
