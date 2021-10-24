96d4075e104f07d0f34f600b8a4d37790fea0d48
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-10-24 13:06:21 -0300

    Recorrido de Slices con Range

--- src/main.go
+++ src/main.go
@@ -3,11 +3,5 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Println("============== Ejercicio 1 - Recorrido de Slices con Range ==================")
-	slice := []string{"Hola", "que", "tal", "?"}
-
-	for indice, value := range slice {
-		fmt.Println(indice, value)
-	}
+	fmt.Println("============== Ejercicio 1 -  ==================")
 }
