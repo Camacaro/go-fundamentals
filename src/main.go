9eb71b874abaa93e34c7c9add4c07fdbcd9f3637
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-10-24 12:22:58 -0300

    Arrays y Slices

--- src/main.go
+++ src/main.go
@@ -4,5 +4,10 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("============== Ejercicio 1 -  ==================")
+	fmt.Println("============== Ejercicio 1 - Recorrido de Slices con Range ==================")
+	slice := []string{"Hola", "que", "tal", "?"}
+
+	for indice, value := range slice {
+		fmt.Println(indice, value)
+	}
 }
