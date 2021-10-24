cbafb53bfe9d5efdb2bc714cbf4aa94242a324e3
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-10-24 14:50:32 -0300

    Structs and class

--- src/main.go
+++ src/main.go
@@ -2,52 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("============== Ejercicio 1 -  ==================")
-}
-package main
-
-import "fmt"
-
-func closeDb() {
-	fmt.Println("close db")
+type car struct {
+	brand string
+	year  int
 }
 
 func main() {
 
-	fmt.Println("============== Ejecicio 1 - El uso de los keywords defer ==================")
-	// Antes de morir el codigo se ejecuta el defer
-	// Lo que hace es ejecutar la ultima funcion antes de que termine el programa
-	// Case de uso que cierre la coneccion a la base de datos
-	// Case de uso que un cierre el archivo al terminar el programa
-	// Las buenas practicas dicen que solo se debe usar un defer por programa
-	// La secuencia con mas de un defer es de la ultima a la primera
+	// Sobre la definición, un struct es simplemente una colección de campos.
+	fmt.Println("============== Ejercicio 1 - Structs: La forma de hacer clases en Go  ==================")
+	myCar := car{brand: "Ford", year: 2020}
+	fmt.Println("myCar", myCar)
 
-	// Antes de probrar el ejemplo de defer, comenta el otro ejercicio
-	// defer fmt.Println("!!!")  // Se ejecuta de tercero
-	// defer fmt.Println("Hola") // Se ejecuta segundo
-	// fmt.Println("Mundo")      // Se ejecuta primero
-
-	fmt.Println("============== Ejecicio 2 - El uso de los keywords continue y break ==================")
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
-
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
-	}
-
-fmt.Println("============== Reto - Close DB ==================")
-defer closeDb()
-fmt.Println("main")
-fmt.Println("Se abre conexion db")
-fmt.Println("Se guargo item en db")
-fmt.Println("Se retorna valor")
-
+	fmt.Println("============== Ejercicio 2 - Otra manera de instanciar  ==================")
+	var otherCar car
+	otherCar.brand = "Ferrari"
+	// Aqui me imprime el year como 0 por su valor por defecto, zero value int
+	fmt.Println("otherCar", otherCar)
 }
