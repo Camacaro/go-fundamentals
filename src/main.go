83358061701224c71f428e13f354dbb5cf7fb44f
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-10-24 13:21:58 -0300

    Map, dictionario or objects

--- src/main.go
+++ src/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("============== Ejercicio 1 -  ==================")
+
+	fmt.Println("============== Ejercicio 1 - Llave valor con Maps  ==================")
 }
