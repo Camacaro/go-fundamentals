7a40a4a04ab2254f0a731881a8c4b68a8e6820a2
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-10-25 09:28:45 -0300

    Stringers: personalizar el output de Structs

--- src/main.go
+++ src/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+type pc struct {
+	ram   int
+	brand string
+	disk  int
+}
+
+// La estructura de datos " Struct " tiene un método llamado
+// " String " , que podemos sobrescribir para personalizar
+// la salida a consola de los datos del struct.
+func (myPc pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPc.ram, myPc.disk, myPc.brand)
+}
+
 func main() {
-	
+	myPc := pc{ram: 8, brand: "Dell", disk: 500}
+
+	// Se imprimira de otra forma
+	fmt.Println(myPc)
 }
